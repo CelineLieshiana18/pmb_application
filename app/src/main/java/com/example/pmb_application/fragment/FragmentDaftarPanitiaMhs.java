e298d1101f1cdb63691ab6fe03604f95db40d1dd
Bryan Wijaya <bryblackhawk008@gmail.com>
2020-11-23 15:39:45 +0700

    update 23112020
    cek login student, benerin get data panitia sama mhs

--- app/src/main/java/com/example/pmb_application/fragment/FragmentDaftarPanitiaMhs.java
+++ app/src/main/java/com/example/pmb_application/fragment/FragmentDaftarPanitiaMhs.java
@@ -1,66 +1,92 @@
 package com.example.pmb_application.fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.pmb_application.R;
+import com.example.pmb_application.VariabelGlobal;
+import com.example.pmb_application.adapter.PanitiaAdapter;
+import com.example.pmb_application.databinding.FragmentDaftarPanitiaMhsBinding;
+import com.example.pmb_application.databinding.FragmentKelolaPanitiaBinding;
+import com.example.pmb_application.entity.Student;
+import com.example.pmb_application.entity.WSResponseMhs;
+import com.google.gson.Gson;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link FragmentDaftarPanitiaMhs#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FragmentDaftarPanitiaMhs extends Fragment {
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FragmentDaftarPanitiaMhs() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment daftarPanitiaMhs.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentDaftarPanitiaMhs newInstance(String param1, String param2) {
-        FragmentDaftarPanitiaMhs fragment = new FragmentDaftarPanitiaMhs();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+public class FragmentDaftarPanitiaMhs extends Fragment implements PanitiaAdapter.ItemClickListener{
+    private FragmentDaftarPanitiaMhsBinding binding;
+    private PanitiaAdapter panitiaAdapter;
+    String URLGET = VariabelGlobal.link_ip + "api/students/getPanitia/";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        loadPanitiaData();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daftar_panitia_mhs, container, false);
+        binding = FragmentDaftarPanitiaMhsBinding.inflate(inflater,container,false);
+        initComponents();
+        return binding.getRoot();
+    }
+
+    private void  initComponents(){
+        binding.rvDataDaftarPanitiaMhs.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rvDataDaftarPanitiaMhs.setAdapter(getPanitiaAdapter());
+        binding.srLayoutDaftarPanitiaMhs.setOnRefreshListener(()->{
+            binding.srLayoutDaftarPanitiaMhs.setRefreshing(false);
+            loadPanitiaData();
+        });
+    }
+
+    public PanitiaAdapter getPanitiaAdapter() {
+        if (panitiaAdapter == null){
+            panitiaAdapter = new PanitiaAdapter(this);
+        }
+        return panitiaAdapter;
+    }
+
+
+    private void loadPanitiaData() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        Uri uri = Uri.parse(URLGET).buildUpon().build();
+        System.out.println(uri);
+        StringRequest request = new StringRequest(Request.Method.GET, uri.toString(), response -> {
+            try {
+                JSONObject object = new JSONObject(response);
+                Gson gson = new Gson();
+                WSResponseMhs weatherResponse = gson.fromJson(object.toString(), WSResponseMhs.class);
+                getPanitiaAdapter().changeData(weatherResponse.getData());
+                Toast.makeText(getActivity(), "berhasil",Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            Toast.makeText(getActivity(),"error", Toast.LENGTH_SHORT).show();
+            error.printStackTrace();
+        });
+        queue.add(request);
+    }
+
+    @Override
+    public void itemClicked(Student student) {
+
     }
 }
--- app/src/main/java/com/example/pmb_application/fragment/FragmentDaftarTemanMhs.java
+++ app/src/main/java/com/example/pmb_application/fragment/FragmentDaftarTemanMhs.java
@@ -1,26 +1,92 @@
 package com.example.pmb_application.fragment;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.pmb_application.R;
+import com.example.pmb_application.VariabelGlobal;
+import com.example.pmb_application.adapter.MhsAdapter;
+import com.example.pmb_application.databinding.FragmentDaftarTemanMhsBinding;
+import com.example.pmb_application.entity.Student;
+import com.example.pmb_application.entity.WSResponseLoginStudent;
+import com.google.gson.Gson;
 
-public class FragmentDaftarTemanMhs extends Fragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FragmentDaftarTemanMhs extends Fragment implements MhsAdapter.ItemClickListener{
+    private FragmentDaftarTemanMhsBinding binding;
+    String URLGET = VariabelGlobal.link_ip + "api/students/getMahasiswa/";
+    private MhsAdapter mahasiswaAdapter;
+
+    public MhsAdapter getMahasiswaAdapter() {
+        if(mahasiswaAdapter == null){
+            mahasiswaAdapter = new MhsAdapter(this);
+        }
+        return mahasiswaAdapter;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadMhsData();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_daftar_teman_mhs, container, false);
+        binding = FragmentDaftarTemanMhsBinding.inflate(inflater,container,false);
+        initComponents();
+        return binding.getRoot();
+    }
+
+
+    private void  initComponents(){
+        binding.rvDataDaftarTemanMhs.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.rvDataDaftarTemanMhs.setAdapter(getMahasiswaAdapter());
+        binding.srLayoutDaftarTemanMhs.setOnRefreshListener(()->{
+            binding.srLayoutDaftarTemanMhs.setRefreshing(false);
+            loadMhsData();
+        });
+    }
+
+    private void loadMhsData() {
+        RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        Uri uri = Uri.parse(URLGET).buildUpon().build();
+        System.out.println(uri);
+        StringRequest request = new StringRequest(Request.Method.GET, uri.toString(), response -> {
+            try {
+                JSONObject object = new JSONObject(response);
+                Gson gson = new Gson();
+                WSResponseLoginStudent weatherResponse = gson.fromJson(object.toString(), WSResponseLoginStudent.class);
+                getMahasiswaAdapter().changeData(weatherResponse.getData());
+                Toast.makeText(getActivity(), "berhasil",Toast.LENGTH_SHORT).show();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }, error -> {
+            Toast.makeText(getActivity(),"error", Toast.LENGTH_SHORT).show();
+            error.printStackTrace();
+        });
+        queue.add(request);
+    }
+
+
+    @Override
+    public void itemClicked(Student student) {
+
     }
 }
